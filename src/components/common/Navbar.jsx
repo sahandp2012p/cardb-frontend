8248c23780bbb4879fc75e2f06a601d167577ee1
Sahand Pourjavad <spourjavad2012@gmail.com>
2022-05-07 14:50:44 +0430

    Finished navbar component

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
 export default () => {
+  const handleChange = () => {
+    document.querySelector("nav ul").classList.toggle("closed")
+    document.querySelector("nav").classList.toggle("open")
+  }
+
   return (
     <nav>
-        <div className="header">CarDB</div>
-        <div className="hamburger">
-            <span className="line"></span>
-            <span className="line"></span>
-            <span className="line"></span>
+        <div className="header-wrap">
+          <div className="header">CarDB</div>
+          <div className="hamburger" onClick={handleChange}>
+              <span className="line"></span>
+              <span className="line"></span>
+              <span className="line"></span>
+          </div>
         </div>
 
         <ul className='closed'>
