841b77821967919d619c94990ec87b7744fdd8d9
Sahand Pourjavad <spourjavad2012@gmail.com>
2022-05-07 11:54:36 +0430

    Created navbar component without styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/common/Navbar';
 
 function App() {
   return (
     <>
-      
+      <Navbar />
     </>
   );
 }
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <nav>
+        <div className="header">CarDB</div>
+        <div className="hamgurger"></div>
+
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Sign up</a></li>
+        </ul>
+    </nav>
+  )
+}
